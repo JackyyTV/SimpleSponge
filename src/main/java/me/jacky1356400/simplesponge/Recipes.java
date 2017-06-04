8e2d259505b28e315a3a8ff6cc4bdaadf8a272e3
Jacky1356400 <jacky1356400@hotmail.com>
2017-06-04 20:47:29 +0800

    Final preprations for release

--- src/main/java/me/jacky1356400/simplesponge/Recipes.java
+++ src/main/java/me/jacky1356400/simplesponge/Recipes.java
@@ -6,13 +6,15 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
 
     public static void init(){
         ShapelessOreRecipe spongeRecipe = new ShapelessOreRecipe(new ItemStack(SimpleSpongeBlocks.sponge), Blocks.WOOL, "slimeball");
         GameRegistry.addRecipe(spongeRecipe);
-        GameRegistry.addShapedRecipe(new ItemStack(SimpleSpongeItems.spongeOnAStick), "  s", " w ", "w  ", 's', SimpleSpongeBlocks.sponge, 'w', Items.STICK);
+        ShapedOreRecipe spongeonastickRecipe = new ShapedOreRecipe(new ItemStack(SimpleSpongeItems.spongeOnAStick), " s ", " w ", " w ", 's', SimpleSpongeBlocks.sponge, 'w', "stickWood");
+        GameRegistry.addRecipe(spongeonastickRecipe);
     }
 
 }
