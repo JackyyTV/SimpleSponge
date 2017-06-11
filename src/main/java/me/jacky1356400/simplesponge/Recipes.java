6d6a4ff5dc6e657f2801670236e0cc8dc9471da7
ThatGamerBlue <thatgamerblue@gmail.com>
2017-06-11 21:40:43 +0100

    Magmatics now listen to config and have a recipe

--- src/main/java/me/jacky1356400/simplesponge/Recipes.java
+++ src/main/java/me/jacky1356400/simplesponge/Recipes.java
@@ -1,18 +1,31 @@
 package me.jacky1356400.simplesponge;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class Recipes {
 
     public static void init(){
-        ShapelessOreRecipe spongeRecipe = new ShapelessOreRecipe(new ItemStack(SimpleSpongeBlocks.sponge), Blocks.WOOL, "slimeball");
+        ShapelessOreRecipe spongeRecipe = new ShapelessOreRecipe(new ItemStack(SimpleSpongeBlocks.sponge),
+                new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE), "slimeball");
         GameRegistry.addRecipe(spongeRecipe);
-        ShapedOreRecipe spongeonastickRecipe = new ShapedOreRecipe(new ItemStack(SimpleSpongeItems.spongeOnAStick), " s ", " w ", " w ", 's', SimpleSpongeBlocks.sponge, 'w', "stickWood");
-        GameRegistry.addRecipe(spongeonastickRecipe);
+
+        ShapedOreRecipe spongeOnAStickRecipe = new ShapedOreRecipe(new ItemStack(SimpleSpongeItems.spongeOnAStick),
+                " s ", " w ", " w ", 's', SimpleSpongeBlocks.sponge, 'w', "stickWood");
+        GameRegistry.addRecipe(spongeOnAStickRecipe);
+
+        ShapelessOreRecipe magmaticSpongeRecipe = new ShapelessOreRecipe(new ItemStack(SimpleSpongeBlocks.magmaticSponge),
+                new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE), Items.MAGMA_CREAM);
+        GameRegistry.addRecipe(magmaticSpongeRecipe);
+
+        ShapedOreRecipe magmaticSpongeOnAStickRecipe = new ShapedOreRecipe(new ItemStack(SimpleSpongeItems.magmaticSpongeOnAStick),
+                " s ", " w ", " w ", 's', SimpleSpongeBlocks.magmaticSponge, 'w', "stickWood");
+        GameRegistry.addRecipe(magmaticSpongeOnAStickRecipe);
     }
 
 }
