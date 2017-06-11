6bc07a479a26849f0eb8320960636f7646123983
ThatGamerBlue <thatgamerblue@gmail.com>
2017-06-11 18:22:40 +0100

    Any wool color.

--- src/main/java/me/jacky1356400/simplesponge/Recipes.java
+++ src/main/java/me/jacky1356400/simplesponge/Recipes.java
@@ -1,13 +1,14 @@
 package me.jacky1356400.simplesponge;
 
-import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class Recipes {
 
     public static void init(){
-        Block W = Blocks.WOOL;
+        ItemStack W = new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE);
         Item S = Item.getItemFromBlock(SimpleSpongeBlocks.sponge);
         String T = "stickWood";
         String B = "slimeball";
