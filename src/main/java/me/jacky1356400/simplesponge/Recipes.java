104f5441c2de7449a496053d18e0a2c4cff4231f
ThatGamerBlue <thatgamerblue@gmail.com>
2017-06-15 14:06:48 +0100

    Recipes and build.gradle updates

--- src/main/java/me/jacky1356400/simplesponge/Recipes.java
+++ src/main/java/me/jacky1356400/simplesponge/Recipes.java
@@ -1,20 +1,19 @@
 package me.jacky1356400.simplesponge;
 
 import net.minecraft.client.gui.recipebook.RecipeList;
-import net.minecraft.client.util.RecipeBookClient;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.oredict.OreDictionary;
-
-import java.util.List;
 
 public class Recipes {
 
-    private static List<RecipeList> recipes = RecipeBookClient.field_194086_e.get(CreativeTabs.TOOLS);
     private static RecipeList list = new RecipeList();
+
+    private static int id = 0;
 
     public static void init(){
         ItemStack W = new ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE);
@@ -23,17 +22,16 @@ public class Recipes {
         Item MS = Item.getItemFromBlock(SimpleSpongeBlocks.magmaticSponge);
         String T = "stickWood";
         String B = "slimeball";
-        list.func_192709_a(RecipeHelper.addShapeless(SimpleSpongeBlocks.sponge,
-                new Object[] {W, B}));
-        list.func_192709_a(RecipeHelper.addShaped(SimpleSpongeItems.spongeOnAStick, 3, 3,
-                new Object[] {null, S, null, null, T, null, null, T, null}));
-        list.func_192709_a(RecipeHelper.addShapeless(SimpleSpongeBlocks.magmaticSponge,
-                new Object[] {S, M}));
-        list.func_192709_a(RecipeHelper.addShaped(SimpleSpongeItems.magmaticSpongeOnAStick, 3, 3,
-                new Object[] {null, MS, null, null, T, null, null, T, null}));
-
-        // Keep this last
-        recipes.add(list);
+        CraftingManager.REGISTRY.register(id++, new ResourceLocation(SimpleSponge.MODID + ":recipe" + id),
+                RecipeHelper.getShapeless(SimpleSpongeBlocks.sponge, new Object[] {W, B}));
+        CraftingManager.REGISTRY.register(id++, new ResourceLocation(SimpleSponge.MODID + ":recipe" + id),
+                RecipeHelper.getShapeless(SimpleSpongeBlocks.magmaticSponge, new Object[] {S, M}));
+        CraftingManager.REGISTRY.register(id++, new ResourceLocation(SimpleSponge.MODID + ":recipe" + id),
+                RecipeHelper.getShaped(new ItemStack(SimpleSpongeItems.spongeOnAStick, 1, 0),
+                        3, 3, new Object[] {null, S, null, null, T, null, null, T, null}));
+        CraftingManager.REGISTRY.register(id++, new ResourceLocation(SimpleSponge.MODID + ":recipe" + id),
+                RecipeHelper.getShaped(new ItemStack(SimpleSpongeItems.magmaticSpongeOnAStick,1, 0),
+                        3, 3, new Object[] {null, MS, null, null, T, null, null, T, null}));
     }
 
 }
